f9e2378bdd1b9b4b27e69458b24189b8095f952e
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2025-06-08 20:57:28 +0100

    adding header and footer

---
+++ partials/footer.php
@@ -0,0 +1,96 @@
+<!-- Footer Start -->
+<div
+    class="container-fluid bg-dark text-white-50 footer mt-5 pt-5 wow fadeIn"
+    data-wow-delay="0.1s">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-3 col-md-6">
+                <div class="d-flex align-items-center">
+                    <img src="img/logo.png" alt="" width="55px" height="50px" />
+                    <h1 class="fw-bold text-primary m-0">
+                        Period<span class="text-white">Slay</span>
+                    </h1>
+                </div>
+                <p>
+                    A world where menstruation is celebrated, empowering girls and
+                    women to harness their cycles as a source of
+                    strength and positivity.
+                </p>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-square me-1" href="https://www.facebook.com/share/12JchYDaK2A/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <!-- <a class="btn btn-square me-1" href=""><i class="fab fa-twitter"></i></a> -->
+                    <a class="btn btn-square me-1" href="https://www.linkedin.com/company/106284737/admin/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                    <a
+                        class="btn btn-square me-1"
+                        href="https://www.instagram.com/periodslayinitiative?igsh=bWNlY3BxYmF5cGZz"
+                        target="_blank"><i class="fab fa-instagram"></i></a>
+                    <a
+                        class="btn btn-square me-1"
+                        href="https://youtube.com/@periodslaytv?si=Y-VRVCNirVKOcTVA"
+                        target="_blank"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Address</h5>
+                <p><i class="fa fa-map-marker-alt me-3"></i>Enugu, Nigeria</p>
+                <p><i class="fa fa-phone-alt me-3"></i>+234 813 282 5123</p>
+                <p><i class="fa fa-envelope me-3"></i>support@periodslay.org</p>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Quick Links</h5>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">Our Services</a>
+                <a class="btn btn-link" href="">Terms & Condition</a>
+                <a class="btn btn-link" href="">Supprt</a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Newsletter</h5>
+                <p>Subscribe to our newsletter</p>
+                <div class="position-relative mx-auto" style="max-width: 400px">
+                    <input
+                        class="form-control bg-transparent w-100 py-3 ps-4 pe-5"
+                        type="text"
+                        placeholder="Your email" />
+                    <button
+                        type="button"
+                        class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">
+                        SignUp
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a href="#">Period Slay</a>, All Right Reserved.
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                    <a href="/privacy">Privacy Notice</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="lib/wow/wow.min.js"></script>
+<script src="./lib/easing/easing.min.js"></script>
+<script src="./lib/waypoints/waypoints.min.js"></script>
+<script src="./lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="./lib/parallax/parallax.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="./js/main.js"></script>
+</body>
+
+</html>
